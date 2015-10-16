d87fab74e41acca0f18fd946d4799ed682057bc3
Gabriel Gomez <ggomez@GGomez-NTB.treorbis.local>
2015-10-16 11:07:23 +0200

    Update of filter V01

--- view/Detail.controller.js
+++ view/Detail.controller.js
@@ -1,14 +1,18 @@
+var Glob_T;
+var Glob_F;
+var Glob_K;
+
 sap.ui.core.mvc.Controller.extend("Variantconf.view.Detail", {
 
-	onInit : function() {
+	onInit: function() {
 		this.oInitialLoadFinishedDeferred = jQuery.Deferred();
 
-		if(sap.ui.Device.system.phone) {
+		if (sap.ui.Device.system.phone) {
 			//Do not wait for the master when in mobile phone resolution
 			this.oInitialLoadFinishedDeferred.resolve();
 		} else {
 			this.getView().setBusy(true);
-			var oEventBus = this.getEventBus(); 
+			var oEventBus = this.getEventBus();
 			oEventBus.subscribe("Component", "MetadataFailed", this.onMetadataFailed, this);
 			oEventBus.subscribe("Master", "InitialLoadFinished", this.onMasterLoaded, this);
 		}
@@ -16,25 +20,25 @@ sap.ui.core.mvc.Controller.extend("Variantconf.view.Detail", {
 		this.getRouter().attachRouteMatched(this.onRouteMatched, this);
 	},
 
-	onMasterLoaded :  function (sChannel, sEvent) {
+	onMasterLoaded: function(sChannel, sEvent) {
 		this.getView().setBusy(false);
 		this.oInitialLoadFinishedDeferred.resolve();
-	},
-	
-	onMetadataFailed : function(){
-		this.getView().setBusy(false);
-		this.oInitialLoadFinishedDeferred.resolve();
-        this.showEmptyView();	    
 	},
 
-	onRouteMatched : function(oEvent) {
+	onMetadataFailed: function() {
+		this.getView().setBusy(false);
+		this.oInitialLoadFinishedDeferred.resolve();
+		this.showEmptyView();
+	},
+
+	onRouteMatched: function(oEvent) {
 		var oParameters = oEvent.getParameters();
 
-		jQuery.when(this.oInitialLoadFinishedDeferred).then(jQuery.proxy(function () {
+		jQuery.when(this.oInitialLoadFinishedDeferred).then(jQuery.proxy(function() {
 			var oView = this.getView();
 
 			// When navigating in the Detail page, update the binding context 
-			if (oParameters.name !== "detail") { 
+			if (oParameters.name !== "detail") {
 				return;
 			}
 
@@ -43,14 +47,16 @@ sap.ui.core.mvc.Controller.extend("Variantconf.view.Detail", {
 
 			var oIconTabBar = oView.byId("idIconTabBar");
 			oIconTabBar.getItems().forEach(function(oItem) {
-			    if(oItem.getKey() !== "selfInfo"){
-    				oItem.bindElement(oItem.getKey());
-			    }
+				if (oItem.getKey() !== "selfInfo") {
+					oItem.bindElement(oItem.getKey());
+				}
 			});
 
 			// Specify the tab being focused
 			var sTabKey = oParameters.arguments.tab;
-			this.getEventBus().publish("Detail", "TabChanged", { sTabKey : sTabKey });
+			this.getEventBus().publish("Detail", "TabChanged", {
+				sTabKey: sTabKey
+			});
 
 			if (oIconTabBar.getSelectedKey() !== sTabKey) {
 				oIconTabBar.setSelectedKey(sTabKey);
@@ -59,12 +65,12 @@ sap.ui.core.mvc.Controller.extend("Variantconf.view.Detail", {
 
 	},
 
-	bindView : function (sEntityPath) {
+	bindView: function(sEntityPath) {
 		var oView = this.getView();
-		oView.bindElement(sEntityPath); 
+		oView.bindElement(sEntityPath);
 
 		//Check if the data is already on the client
-		if(!oView.getModel().getData(sEntityPath)) {
+		if (!oView.getModel().getData(sEntityPath)) {
 
 			// Check that the entity specified was found.
 			oView.getElementBinding().attachEventOnce("dataReceived", jQuery.proxy(function() {
@@ -83,30 +89,32 @@ sap.ui.core.mvc.Controller.extend("Variantconf.view.Detail", {
 
 	},
 
-	showEmptyView : function () {
-		this.getRouter().myNavToWithoutHash({ 
-			currentView : this.getView(),
-			targetViewName : "Variantconf.view.NotFound",
-			targetViewType : "XML"
+	showEmptyView: function() {
+		this.getRouter().myNavToWithoutHash({
+			currentView: this.getView(),
+			targetViewName: "Variantconf.view.NotFound",
+			targetViewType: "XML"
 		});
 	},
 
-	fireDetailChanged : function (sEntityPath) {
-		this.getEventBus().publish("Detail", "Changed", { sEntityPath : sEntityPath });
+	fireDetailChanged: function(sEntityPath) {
+		this.getEventBus().publish("Detail", "Changed", {
+			sEntityPath: sEntityPath
+		});
 	},
 
-	fireDetailNotFound : function () {
+	fireDetailNotFound: function() {
 		this.getEventBus().publish("Detail", "NotFound");
 	},
 
-	onNavBack : function() {
+	onNavBack: function() {
 		// This is only relevant when running on phone devices
 		this.getRouter().myNavBack("main");
 	},
 
-	onDetailSelect : function(oEvent) {
-		sap.ui.core.UIComponent.getRouterFor(this).navTo("detail",{
-			entity : oEvent.getSource().getBindingContext().getPath().slice(1),
+	onDetailSelect: function(oEvent) {
+		sap.ui.core.UIComponent.getRouterFor(this).navTo("detail", {
+			entity: oEvent.getSource().getBindingContext().getPath().slice(1),
 			tab: oEvent.getParameter("selectedKey")
 		}, true);
 	},
@@ -120,21 +128,62 @@ sap.ui.core.mvc.Controller.extend("Variantconf.view.Detail", {
 			this._oActionSheet.setShowCancelButton(true);
 			this._oActionSheet.setPlacement(sap.m.PlacementType.Top);
 		}
-		
+
 		this._oActionSheet.openBy(this.getView().byId("actionButton"));
 	},
 
-	getEventBus : function () {
+	getEventBus: function() {
 		return sap.ui.getCore().getEventBus();
 	},
 
-	getRouter : function () {
+	getRouter: function() {
 		return sap.ui.core.UIComponent.getRouterFor(this);
 	},
-	
-	onExit : function(oEvent){
-	    var oEventBus = this.getEventBus();
-    	oEventBus.unsubscribe("Master", "InitialLoadFinished", this.onMasterLoaded, this);
+
+	onChange_T: function(oEvent) {
+		var MyVariable = oEvent.getParameter("selectedItem").getKey();
+		Glob_T = MyVariable;
+	},
+	onChange_F: function(oEvent) {
+		var MyVariable = oEvent.getParameter("selectedItem").getKey();
+		Glob_F = MyVariable;
+	},
+	onChange_K: function(oEvent) {
+		var MyVariable = oEvent.getParameter("selectedItem").getKey();
+		Glob_K = MyVariable;
+	},
+
+	onSelectChanged: function(oEvent) {
+
+		var oCrit = "SKRIT_FIORI_PRG=" + "BA" + "___SKRIT_FIORI_TYP=" + Glob_T + "___SKRIT_FIORI_FARBE=" + Glob_F;
+
+		var key = oEvent.getParameters().key;
+		if (key === '2') {
+			var oFilter_T = new sap.ui.model.Filter("ATNAM", "EQ", "FIORI_TYP");
+			var oBinding_T = this.byId("Typ").getBinding("items");
+			oBinding_T.filter([oFilter_T]);
+		} else if (key === '3') {
+			var oFilter_F = new sap.ui.model.Filter("ATNAM", "EQ", "FIORI_FARBE");
+			var oBinding_F = this.byId("Farbe").getBinding("items");
+			oBinding_F.filter([oFilter_F]);
+		} else if (key === '4') {
+			var oFilter1 = {};
+			oFilter1 = new sap.ui.model.Filter("ATNAM", sap.ui.model.FilterOperator.EQ, "FIORI_BEZUGSSTOFF");
+			var oFilter2 = {};
+			oFilter2 = new sap.ui.model.Filter("ATWRT", sap.ui.model.FilterOperator.EQ, oCrit);
+			var allFilter = new sap.ui.model.Filter([oFilter1, oFilter2], true);
+			var oBinding2 = this.byId("Stoff").getBinding("items");
+			oBinding2.filter([allFilter]);
+		} else if (key === '5') {
+			var oFilter_K = new sap.ui.model.Filter("ATNAM", "EQ", "FIORI_KISSEN_ANZ");
+			var oBinding_K = this.byId("Kissen").getBinding("items");
+			oBinding_K.filter([oFilter_K]);
+		}
+	},
+
+	onExit: function(oEvent) {
+		var oEventBus = this.getEventBus();
+		oEventBus.unsubscribe("Master", "InitialLoadFinished", this.onMasterLoaded, this);
 		oEventBus.unsubscribe("Component", "MetadataFailed", this.onMetadataFailed, this);
 		if (this._oActionSheet) {
 			this._oActionSheet.destroy();
